a2b9ea1ea59d54a8e8e2347cfbdeb4d68526bbba
Tarun Bhardwaj <tarun_bhardwaj@hotmail.com>
2022-11-19 17:13:46 +0530

    Latest changes

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	database "github.com/SampleGitProject/internal"
+)
 
 func main() {
+	database.Db()
 	fmt.Println("Hello world!")
+	fmt.Println("Another Change")
 }
